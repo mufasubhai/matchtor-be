2caecda9944eae53e25cf866bcff8d6152483318
Adrian Apodaca <aapodaca@gmail.com>
2021-01-31 08:14:20 -0800

    begin adding express routes

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Hello World"));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 const mongoose = require('mongoose');
 const db = require('./config/keys_dev').mongoURI;
+const bodyParser = require('body-parser');
+
+
+
+//routes
+const users = require("./routes/api/users");
+
+const tasks = require("./routes/api/tasks");
+
+app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
+app.use("/api/tasks", tasks);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));xc
+  .catch(err => console.log(err));
